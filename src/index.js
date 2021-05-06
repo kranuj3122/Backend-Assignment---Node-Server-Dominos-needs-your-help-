292739448550d092688d698ad49704598b5837ed
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-05-06 23:41:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+    if(req.url == '/welcome') {
+        res.writeHead(200,{"Contebt-type":"text/plain"});
+        res.end("welcome to Dominos!");
+    }
+    else if(req.url == '/contact') {
+        res.writeHead(200,{"Content-type":"application/json"});
+        res.end({
+                    "phone": '18602100000',
+                    "email": 'guestcaredominos@jublfood.com'
+                });
+    }
+    else {
+        res.writeHead(404,{"Content-type":"text/plain"});
+        res.end();
+    }
 }
+
+httpServer.listen(8081);
 
 module.exports = httpServer;
